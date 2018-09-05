f4a362ca7cfa1859b36994b04f3b851c06f6b6c4
CADeer <christinedeer@gmail.com>
2018-09-05 11:50:32 -0400

    changes to index and js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,39 +2,70 @@
 
 
 ///Variable array of Alphabet Choices for Computer
-var letterChoices = ["a","b","c","d","e","f","g","h","i","g","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var computerLetterChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 
 ///Wins Losses and Guess Left variables
 var wins = 0;
 var losses = 0;
 var guessLeft = 9;
-var userList = [];
+var UsedCharacterList = [];
 
-///When User presses key on keyboard make sure it is a **CHARACTER** and make any input lowercase
+
 
 
 ///Computer picks from LetterChoices randomly
 
 
+var computerPicksLetter = computerLetterChoices[Math.floor(Math.random() * computerLetterChoices.length)];
+
+
+///Display Text on HTML Document
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessText = document.getElementById("guess-Text");
+var userChoiceText = document.getElementById("userChoice-Text");
+
+
+///When User presses key on keyboard make sure it is a **CHARACTER** and make any input lowercase
+document.onkeyup = function(event) {
+
+        // word = word + String.fromCharCode(key.keyCode).toLowerCase();
+        var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
 ///USER VRS COMPUTER
 
-    ///// If User's guess is the same as computer's choise 
-            //// Increment wins counter by one 
-            //// And start the guess left to 9 again
-            //// Add wins to wins-text
-            //// Reset userList
 
     //// If User's guess is not the same as the computer's choise and the guessLeft is larger than 0 then 
-    
-            //// Decrease one from guessLeft 
-            //// add guessLeft to guessText
-            //// And add the letter the user guessed to the userList
-            //// In userList add userGuess and show on userguess-text
 
-    //// If User's guess is not the same as the computer's choise and the guessLeft is equal to 0 then 
+    if (UsedCharacterList.indexOf(userGuess) < 0 && computerLetterChoices.indexOf(userGuess) >= 0) {
+        
+        //// In userCharacterList add userGuess
+        UsedCharacterList[UsedCharacterList.length]=userGuess; 
+        guessLeft--; //// Decrease one from guessLeft
+}
+    ///// If User's guess is the same as computer's choise 
+
+    if (computerPicksLetter == userGuess) {
+        wins++; //// Increment wins counter by one 
+        guessLeft = 9; //// And start the guess left to 9 again
+        UsedCharacterList = []; //// Reset user's list
+}
+            
+    //// GuessLeft is equal to 0 then 
     
-            //// Increment losses by one    
-            //// Add losses to losses-text
-            //// Set guessLeft to 9
-            //// Reset userList
+    if (guessLeft == 0) {
+        losses++; //// Increment losses by one 
+        guessLeft = 9; //// Set guessLeft to 9
+        UsedCharacterList = []; //// Reset user's list
+
+}
+               
+///Add the text to the divs
+
+winsText.textContent = wins;                    //// Add wins to wins-text
+lossesText.textContent  = losses;               //// Add losses to losses-text
+guessText.textContent = guessLeft;              //// add guessLeft to guessText
+userChoiceText.textContent  = UsedCharacterList;//// add userChoiceText to UsedCharacterList
+
+}
